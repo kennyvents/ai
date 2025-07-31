9a7ef80bb2e8a68d92db108ca8746df84073c6fb
kenny <vents72o@gmail.com>
2025-07-31 14:04:45 +0500

    v2

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
 from utils.filters import extract_sizes
 
-sizes = extract_sizes('Переход ОЦ 0.7 600х300/700х400')
-print(sizes)
+
+def test1():
+    text = 'Переход-1-150*350/150-300-20-20-оц.-0.7-т2.ш2'
+    print(extract_sizes(text))
+
+def test2():
+    text = 'Переход-100/95-64-50-50-оц.-0.5-з.з'
+    print(extract_sizes(text))
+
+test2()
--- utils/filters.py
+++ utils/filters.py
@@ -1,9 +1,31 @@
 import re
 
 def extract_sizes(text):
-    text = text.lower()
+    type_pattern = re.search(r'-(\d)-', text)
 
-    # 600x300, 600/300
-    size_patterns = re.findall(r'(\d{2,4})[xх*/\-\\](\d{2,4})', text)
-    sizes = set((int(a), int(b)) for a, b in size_patterns)
-    return sizes
+    if type_pattern is None:
+        sizes_pattern = re.search(r'-(\d{2,4})/(\d{2,4})', text)
+        sizes_pattern = [sizes_pattern.group(1), sizes_pattern.group(2)]
+        form = 'круглый'
+
+    else:
+        type_pattern = type_pattern.group(1)
+        sizes_pattern = re.findall(r'(\d{2,4}[*]\d{2,4})', text)
+        form = 'прямоугольный'
+
+        if len(sizes_pattern) != 2:
+            sizes_pattern = re.search(r'(\d{2,4}[*]\d{2,4})/(\d{2,4})', text)
+            sizes_pattern = [sizes_pattern.group(1), sizes_pattern.group(2)]
+            form = 'прямоугольный на круглый'
+
+    l_pattern = re.search(r'-(\d{2,4})-', text).group(1)
+    tire_pattern = re.search(r'-\d{2,4}-(\d{2}-\d{2})-[а-я]', text).group(1)
+
+    size_dict = {
+        'type': type_pattern,
+        'size': sizes_pattern,
+        'l': l_pattern,
+        'tire': tire_pattern,
+        'form': form
+    }
+    return size_dict
